2e99f939067216eda1afab31c43aceb89619ae6d
JoFrhwld <JoFrhwld@gmail.com>
2023-05-29 13:41:51 -0400

    system info

---
+++ system_info.R
@@ -0,0 +1,36 @@
+sessionInfo()
+# R version 4.2.2 (2022-10-31)
+# Platform: aarch64-apple-darwin20 (64-bit)
+# Running under: macOS Ventura 13.4
+#
+# Matrix products: default
+# LAPACK: /Library/Frameworks/R.framework/Versions/4.2-arm64/Resources/lib/libRlapack.dylib
+#
+# locale:
+#   [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+#
+# attached base packages:
+#   [1] stats     graphics  grDevices datasets  utils     methods   base
+#
+# loaded via a namespace (and not attached):
+#   [1] compiler_4.2.2 tools_4.2.2    renv_0.16.0
+
+system("which gfortran")
+# /usr/local/bin/gfortran
+
+system("gfortran --version")
+# GNU Fortran (GCC) 12.2.0
+# Copyright (C) 2022 Free Software Foundation, Inc.
+# This is free software; see the source for copying conditions.  There is NO
+# warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+system("which gcc")
+# /usr/bin/gcc
+
+system("gcc --version")
+# Apple clang version 14.0.3 (clang-1403.0.22.14.1)
+# Target: arm64-apple-darwin22.5.0
+# Thread model: posix
+# InstalledDir: /Library/Developer/CommandLineTools/usr/bin
+
+
